3430825fa8477b63b543a499e3f571de9b809575
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-03-26 22:04:33 -0300

    Final de proyecto (Bug en versión móvil)
    El proyecto se finaliza.
    
    Proyecto finalizado con versión móvil y escritorio.
    
    Creado con sus respectivos diseños responsivos.
    
    Se presenta un Bug en el cual, la versión móvil impide desplegar el menú para navegar por sus opciones.

--- css/script.js
+++ css/script.js
@@ -3,10 +3,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toogle('bx-x');
+menuIcon.onclick = ("click", () => {
+    menuIcon.classList.toogle('menu-icon');
     navbar.classList.toogle('active');
-};
+});
 
 
 
@@ -35,5 +35,34 @@ window.onscroll = () => {
     let header = document.querySelector('header');
 
     header.classList-toogle('sticky', window.scrollY > 100);
-}
 
+    /*#################################################################*/
+    /*----------------Ocultar Menu cuando selecciona opcion----------------------*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+/*#################################################################*/
+/*----------------scroll reveal----------------------*/
+ScrollReveal({
+    /* reset: true,*/
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+
+/*#################################################################*/
+/*----------------Efecto escritura---------------------------------*/
+const typed = new Typed('.multiple-text',{
+    strings: ['Frontend Developer', 'UX/UI Designer', 'Concept Artist', '3D Modeler'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 100,
+    loop: true
+});
